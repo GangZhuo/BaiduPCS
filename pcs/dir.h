969f393293f0a5c8ddce67bfb2f6b9ee8625aba2
Gang Zhuo <gang.zhuo@gmail.com>
2014-01-17 02:33:31 +0800

    完成下载单文件

---
+++ pcs/dir.h
@@ -0,0 +1,35 @@
+#ifndef _DIR_H_
+#define _DIR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct my_dirent my_dirent;
+
+struct my_dirent
+{
+   char		 *path; /* File name */
+   char		 *filename;
+   int		 is_dir; /* File type */
+   time_t	 mtime;
+   int		user_flag;
+   my_dirent *next;
+};
+
+int is_dir_or_file(const char *path);
+my_dirent *list_dir(const char *path, int recursion);
+void my_dirent_destroy(my_dirent *link);
+time_t my_dirent_get_mtime(my_dirent *ent);
+
+int my_dirent_remove(const char *path);
+
+int my_dirent_utime(const char *path, time_t mtime);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif
---
+++ pcs/hashtable.h
@@ -0,0 +1,46 @@
+#ifndef _HASHTABLE_H_
+#define _HASHTABLE_H_
+
+#ifndef HASH_EXTEND_MULTIPLIER
+#define HASH_EXTEND_MULTIPLIER ( 1.75F )
+#endif
+
+typedef struct  hashtable_node hashtable_node;
+struct  hashtable_node { 
+	char			*key;
+	unsigned int	hashA;
+	unsigned int	hashB;
+	void			*value;
+	hashtable_node	*next;
+};
+
+typedef struct hashtable hashtable;
+struct hashtable {
+	int				capacity;
+	int				real_capacity;
+	int				count;
+	hashtable_node	**table;
+	void (*free_value)(void *);
+	int				ignore_case;
+};
+
+typedef struct hashtable_iterater {
+	hashtable		*ht;
+	int				index;
+	hashtable_node	*p;
+} hashtable_iterater;
+
+hashtable *hashtable_create(int capacity, int ignore_case, void (*free_value)(void *));
+void hashtable_destroy(hashtable *ht);
+int hashtable_expand(hashtable *ht, int capacity);
+int hashtable_add(hashtable *ht, const char *key, void *value);
+void *hashtable_get(hashtable *ht, const char *key);
+int hashtable_clear(hashtable *ht);
+
+hashtable_iterater *hashtable_iterater_create(hashtable *ht);
+void hashtable_iterater_destroy(hashtable_iterater *iterater);
+void hashtable_iterater_reset(hashtable_iterater *iterater);
+int hashtable_iterater_next(hashtable_iterater *iterater);
+void *hashtable_iterater_current(hashtable_iterater *iterater);
+
+#endif
