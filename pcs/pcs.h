4f0adb09f8eb2fed7ea6870938a8faa8cbf78626
Gang Zhuo <gang.zhuo@gmail.com>
2014-02-10 02:56:38 -0500

    modify the pcs_cat: the function return the raw data which is come from server
    Former-commit-id: 413cf6153c57c19df8452ac8e7b46a31fd3c9f9b

--- pcs/pcs.h
+++ pcs/pcs.h
@@ -286,11 +286,12 @@ PCS_API PcsPanApiRes *pcs_copy(Pcs handle, PcsSList2 *slist);
 /*
  * 不下载文件，直接获取文本文件的内容
  *   path		待获取的文件或目录，地址需写全，如/temp, /temp/file.txt
+ *   dstsz      获取到的内容的字节长度
  * 成功后返回文本内容
  * 使用完后无需调用pcs_free()释放
  * 失败或无内容则返回 NULL。
  */
-PCS_API const char *pcs_cat(Pcs handle, const char *path);
+PCS_API const char *pcs_cat(Pcs handle, const char *path, size_t *dstsz);
 
 /*
  * 下载文件
