562ca1cf20cccfc722bfda9bf3573f3374969c6f
Gang Zhuo <gang.zhuo@gmail.com>
2014-04-16 05:03:41 -0400

    Delete the dependence on libpcre
    Former-commit-id: cea62ac85ad368d8d11ea2edc9d393dbd7a1697a

--- pcs/pcs_utils.h
+++ pcs/pcs_utils.h
@@ -17,15 +17,6 @@ PCS_API char *pcs_utils_readable_size(double size/*in bytes*/, char *buf, int bu
 PCS_API char *pcs_utils_basedir(const char *path);
 PCS_API char *pcs_utils_filename(const char *path);
 PCS_API int pcs_utils_strcmpi(const char *str1, const char *str2);
-
-/*
- * 正则匹配
- * input  用于匹配的原始字符串
- * input_size 原始字符串长度
- * pattern 匹配模式
- * indies  匹配模式中输出到outputs中的索引数组,以-1结尾
- * pslist  用于接收匹配结果的字符串链表,匹配完成后，将按照indies数组中指定的索引按顺序填充到outputs中
-*/
-PCS_API PcsBool pcs_regex(const char *input, int input_size, const char *pattern, int *indies, PcsSList **pslist);
+PCS_API PcsBool pcs_utils_streq(const char *str1, const char *str2, int len);
 
 #endif
