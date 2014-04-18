c64ab2df97d607d976394245a2657b393dcb74d5
Gang Zhuo <gang.zhuo@gmail.com>
2014-04-18 03:56:35 -0400

    fix error
    Former-commit-id: a26a4cc33014fae27f17e1c8ae559c38fa2ea8e9

--- test/main.c
+++ test/main.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "../pcs/pcs_utils.h"
 #include "shell.h"
+#include "daemon.h"
 
 int main(int argc, char *argv[])
 {
-	int res;
-	res = shell(argc, argv);
-#if defined(WIN32) && defined(_DEBUG)
-	system("pause");
-#endif
-	return res;
+	int res, i;
+	for (i = 0; i < argc; i++) {
+		if (pcs_utils_streq(argv[i], "--config=", 9)) {
+			return daemon(argc, argv);
+		}
+	}
+	return shell(argc, argv);
 }
