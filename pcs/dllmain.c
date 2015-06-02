ed69b8c2fc944d1c32edd320e850d6195c673b6e
Gang Zhuo <gang.zhuo@gmail.com>
2015-06-02 08:44:49 -0400

    test code

--- pcs/dllmain.c
+++ pcs/dllmain.c
@@ -1,3 +1,4 @@
+#include "pcs_defs.h"
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
@@ -16,5 +17,35 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 		break;
 	}
 	return TRUE;
+}
+
+/* 用于测试 */
+typedef struct type_size_t
+{
+	int sz_int;
+	int sz_long;
+	int sz_long_long;
+	int sz_int64_t;
+	int sz_size_t;
+	int sz_curl_off_t;
+	int sz_time_t;
+	int sz_pcs_bool_t;
+
+} type_size_t;
+
+/* 用于测试 */
+PCS_API type_size_t* type_size()
+{
+	static type_size_t ts = {
+		.sz_int = sizeof(int),
+		.sz_long = sizeof(long),
+		.sz_long_long = sizeof(long long),
+		.sz_int64_t = sizeof(int64_t),
+		.sz_size_t = sizeof(size_t),
+		.sz_curl_off_t = sizeof(curl_off_t),
+		.sz_time_t = sizeof(time_t),
+		.sz_pcs_bool_t = sizeof(PcsBool),
+	};
+	return &ts;
 }
 
