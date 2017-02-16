b14d01a34b925d742abceebd928e19e54bf2a086
Gang Zhuo <gang.zhuo@gmail.com>
2017-02-16 01:31:50 -0500

    update user-agent

--- shell.c
+++ shell.c
@@ -39,7 +39,7 @@
 #endif
 #include "shell.h"
 
-#define USAGE "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36"
+#define USAGE "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"
 #define TIMEOUT						60
 #define CONNECTTIMEOUT				10
 #define MAX_THREAD_NUM				100
